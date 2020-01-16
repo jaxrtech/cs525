48b3df03e5f61394e9006e5491ab9654d595dcfa
Chris Morcom <cmorcom@hawk.iit.edu>
2020-01-15 18:17:26 -0600

    completed createPageFile in storage_mgr.c need to finish openPageFile

--- Assignemt 1/storage_mgr.c
+++ Assignemt 1/storage_mgr.c
@@ -7,73 +7,109 @@
 #include "storage_mgr.h"
 
  // NOTE: 'RC' stands for 'Return Code' (see dberror.h)
+ //PAGE SIZE is constant in dberror.h as 4KB
 
 /* manipulating page files */
 
-void initStorageManager (void){
+void initStorageManager (void){ // does nothing for now
 	return;
 }
 
 RC createPageFile (char *fileName){
-	return 0;
+	/* Create a new page file fileName. The initial file size should be one page. 
+	   This method should fill this single page with '\0' bytes. */
+	RC return_code;
+	FILE *file;
+	if ((file = fopen(fileName, "w+"))){ //open file for read+write and check if not exist
+		char *block = malloc(PAGE_SIZE); // create a 4KB block
+		memset(block, '\0', PAGE_SIZE);	//fill block with '\0' bytes
+		fwrite(block, sizeof(char), PAGE_SIZE, file); //write to file (disk)
+		free(block);
+		fclose(file);
+		return_code = RC_OK //code 0
+	} else {
+		return_code = RC_FILE_NOT_FOUND; //error 1
+	}
+
+	return return_code;
 }
 
 RC openPageFile (char *fileName, SM_FileHandle *fHandle){
-	return 0;
+	RC return_code;
+	FILE *file;
+	if((file = fopen(fileName, "r+"))){
+		//do something
+	} else {
+		return_code = RC_FILE_NOT_FOUND; //error 1
+	}
+	return return_code;
 }
 
 RC closePageFile (SM_FileHandle *fHandle){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 RC destroyPageFile (char *fileName){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 /* reading blocks from disc */
 
 RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 int getBlockPos (SM_FileHandle *fHandle){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 /* writing blocks to a page file */
 
 RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 RC appendEmptyBlock (SM_FileHandle *fHandle){
-	return 0;
+	RC return_code;
+	return return_code;
 }
 
 RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle){
-	return 0;
+	RC return_code;
+	return return_code;
 }
