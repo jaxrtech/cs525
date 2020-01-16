08190679e34af0e989f0896d4f060bd2e0f56333
Chris Morcom <cmorcom@hawk.iit.edu>
2020-01-15 20:49:27 -0600

    opening page file method complete (i think)

--- Assignment 1/storage_mgr.c
+++ Assignment 1/storage_mgr.c
@@ -20,7 +20,7 @@ RC createPageFile (char *fileName){
 	   This method should fill this single page with '\0' bytes. */
 	RC return_code;
 	FILE *file;
-	if ((file = fopen(fileName, "w+"))){ //open file for read+write and check if not exist
+	if ((file = fopen(fileName, "w+"))){ //create/overwrite file for read+write and check if not exist
 		char *block = malloc(PAGE_SIZE); // create a 4KB block
 		memset(block, '\0', PAGE_SIZE);	//fill block with '\0' bytes
 		fwrite(block, sizeof(char), PAGE_SIZE, file); //write to file (disk)
@@ -28,7 +28,7 @@ RC createPageFile (char *fileName){
 		fclose(file);
 		return_code = RC_OK //code 0
 	} else {
-		return_code = RC_FILE_NOT_FOUND; //error 1
+		return_code = RC_WRITE_FAILED; //error 3: cannot create new file
 	}
 
 	return return_code;
@@ -37,10 +37,24 @@ RC createPageFile (char *fileName){
 RC openPageFile (char *fileName, SM_FileHandle *fHandle){
 	RC return_code;
 	FILE *file;
-	if((file = fopen(fileName, "r+"))){
-		//do something
+	if(access(fileName, R_OK|W_OK) != -1){ //access() in unistd.h for checking file exists with read+write permissions
+		file = fopen(fileName, "r+") //open file for read+write
+		/*calc fHandle struct attributes*/
+		fseek(file, 0L, SEEK_END); //point to EOF
+		int file_length = ftell(file)+1; //get file size (EOF_point+1 - f_begin_point)
+		int no_pages = file_length/PAGE_SIZE;
+		rewind(file); //point back to file beginning
+
+		/*set fHandle struct attributes*/
+		(*fHandle).fileName = fileName;
+		(*fHandle).totalNumPages = no_pages;
+		(*fHandle).curPagePos = 0; //opening points to first file page
+		//(*fHandle).mgmtInfo = NULL;
+		return_code = RC_OK //code 0
+
 	} else {
 		return_code = RC_FILE_NOT_FOUND; //error 1
+		
 	}
 	return return_code;
 }
