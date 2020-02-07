0b2e3eae3801bfac4d11e3b089c79c953c26e106
Chris Morcom <cmorcom@hawk.iit.edu>
2020-02-07 11:36:14 -0600

    ammended file structs and added error code for threading. also did bm_init and shutdown

--- Assignment 2/buffer_mgr.c
+++ Assignment 2/buffer_mgr.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 
 /* linux specific */
 #include <unistd.h>
@@ -13,26 +14,50 @@
 #include "storage_mgr.h"
 #include "buffer_mgr.h"
 
+SM_FileHandle fHandle;
+
 // Buffer Manager Interface Pool Handling
 RC initBufferPool(
         BM_BufferPool *const bm,
         const char *const pageFileName,
-        const int numPages,
+        const int numPages,             //num of buffer slots
         ReplacementStrategy strategy,
         void *stratData)
 {
+    printf("ASSIGNMENT 2 (Buffer Manager)\n\tCS 525 - SPRING 2020\n\tCHRISTOPHER MORCOM & JOSH BOWDEN\n");
+
     //Store BM_BufferPool Attributes
     bm->pageFile = pageFileName;
-    bm.numPages = numPages;
-    bm.strategy = strategy;
+    bm->numPages = numPages;
+    bm->strategy = strategy;
+    bm->stratData = stratData;
 
-
-    return -1;
+    //set up page directory
+    bm->mgmtData = (struct BP_Metadata *) malloc(sizeof(BP_Metadata));
+    //set up array of pages to hold numPages amount of pages
+    (bm->mgmtData)->pageTable = (struct BM_PageHandle *)malloc(numPages*sizeof(BM_PageHandle));
+    
+    //open pagefile
+    openPageFile((char*)pageFileName, &fHandle); 
+    
+    return RC_OK;
 }
 
 RC shutdownBufferPool(BM_BufferPool *const bm)
 {
-    return -1;
+    forceFlushPool(bm); //write all dirty pages to disk
+    
+    //use for freeing later
+    BP_Metadata *md_tmp = bm->mgmtData;
+    BM_PageHandle *ph_tmp = md_tmp->pageTable;
+    
+    if (md_tmp->refCounter > 0){ return RC_FILE_IN_USE; } //check if any pages are pinned
+
+    //Free buffer manager structs
+    free(ph_tmp);
+    free(md_tmp);
+
+    return RC_OK;
 }
 
 RC forceFlushPool(BM_BufferPool *const bm)
--- Assignment 2/buffer_mgr.h
+++ Assignment 2/buffer_mgr.h
@@ -20,27 +20,29 @@ typedef enum ReplacementStrategy {
 typedef int PageNumber;
 #define NO_PAGE -1
 
-typedef struct BM_BufferPool { //Page Directory
-	char *pageFile;
-	int numPages;
-	ReplacementStrategy strategy;
-	BP_Metadata *mgmtData; // use this one to store the bookkeeping info your buffer
-					// manager needs for a buffer pool
-} BM_BufferPool;
+typedef struct BM_PageHandle { //page dir entry
+	PageNumber pageNum; //page ID (first page is zero)
+	char *data;			//points to page content in memory
+	int dirtyFlag; 		//for clock replacement
+	int refCounter;		//store no. threads on each page (increment before accessing)
+} BM_PageHandle;
 
 typedef struct BP_Metadata //stores infor for page replacement pointed to by mgmtinfo
 {
 	BM_PageHandle *pageTable; //array of pointers to a list of Pages and indices (offset used in FIFO)
-	int LRU_Page;	//store array OFFSET for LRU replacement
-	int LFU_Page;	//store array OFFSET for LFU replacement
-	int refCounter; //num of threads accessing PAGE DIR (increment before accessing)
+	BM_PageHandle* LRU_Page;  //store pointer to LRU Page
+	int LFU_Page;			  //store pointer to LRU Page
+	int refCounter;			  //num of threads accessing PAGE DIR (increment before accessing)
 } BP_Metadata;
 
-typedef struct BM_PageHandle { //page dir entry
-	PageNumber pageNum; //first page is zero (also page ID)
-	char *data;			//points to page content in memory
-	int dirtyFlag; 		//for clock replacement
-} BM_PageHandle;
+typedef struct BM_BufferPool { //Page Directory
+	char *pageFile;
+	int numPages;
+	ReplacementStrategy strategy;
+	void *stratData;	   //strategy data for removal strategy
+	BP_Metadata *mgmtData; // use this one to store the bookkeeping info your buffer
+						   // manager needs for a buffer pool
+} BM_BufferPool;
 
 
 // convenience macros
